88b89dcb6533dbea2c9af6b15c2718721a8e830f
bellissimo <ikromjon.a.t@outlook.com>
2026-05-08 16:51:14 +0500

    Added allowed host

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
+    preview: {
+        allowedHosts: ['localhost:3000', 'localhost:4173', 'pyrus-master.expertly.uz']
+    }
 });
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['localhost:3000', 'localhost:4173', 'pyrus-master.expertly.uz']
+  }
 });
